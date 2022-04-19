a9bd757fb01504a56ccaee342d14d3c6b892f06b
Oleksandr Zaiets <megaalexandros97@gmail.com>
2022-04-19 22:09:49 +0300

    I've done all tasks

--- index.js
+++ index.js
@@ -2,5 +2,14 @@
 //document.querySelectorAll(".selector") - повертає масив елементів які відповідає селектору переданому в аргумент
 //(будьякий елемент).innerHtml - міняє html елемента
 //(будьякий елемент).append - додає до елемента
-//document.createElement("div") - повертає новостворений елемент типу який переданий в якості парамету
+//document.createElement("div") - повертає новостворений елемент типу який переданий в якості параметру
+
+const myTitle = document.querySelector("title")
+myTitle.textContent = "Hello, homy!"
+
+const intro = document.querySelector("th")
+intro.textContent = "Welcome to my party, homies! I'm glad to see my team!"
+
+const image = document.querySelector("img.Gif-animation")
+const table = document.querySelectorAll("td.class-mainTable__content")
 
